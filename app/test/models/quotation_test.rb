4d545471245c4268d28babe19ee5c3a46859a9df
Tanawin Siriwan <tanawin.siriwan@wdc.com>
2024-10-06 00:45:15 +0700

    completed deployment Azure

--- app/test/models/quotation_test.rb
+++ app/test/models/quotation_test.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: quotations
+#
+#  id          :bigint           not null, primary key
+#  author_name :string
+#  quote       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :bigint           not null
+#
+# Indexes
+#
+#  index_quotations_on_category_id  (category_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (category_id => categories.id)
+#
 require "test_helper"
 
 class QuotationTest < ActiveSupport::TestCase
